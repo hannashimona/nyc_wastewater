1a5de128f7b394768c37dc182fa0bc81139f4f50
Michael Piazza <michael.piazza.mp@gmail.com>
2023-09-08 17:38:43 -0700

    Add client

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <>
+      <h1>New York City</h1>
+      <h1>Covid Waste-water tracker</h1>
+    </>
+  )
+}
+const App = () => {
+  return (
+    <div className="flex flex-col bg-background-orange min-h-screen">
+      <Header />
+    </div>
+  )
+}
+
+export default App
